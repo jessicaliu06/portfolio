36b4009d293c8e978d29cca8ad2866f27084116a
Jessica Liu <jessicatracyliu@gmail.com>
2025-05-26 13:27:06 -0700

    resume button

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,9 +1,14 @@
 import { Button, Container, Stack, Text, Title, useMantineTheme } from '@mantine/core';
+import { useState } from 'react';
+
+import glass_apple from '/src/assets/glass_apple.png';
 
 export default function Intro() {
   const theme = useMantineTheme();
+    const [isHovered, setIsHovered] = useState(false);
   
   return (
+    <>
     <Container
       fluid
       id="intro"
@@ -37,18 +42,46 @@ export default function Intro() {
         </Text>
 
         <Button
-          component="a"
-          href="https://jessicaliu06.github.io/resume/LiuJessica_Resume.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            backgroundColor: '#edacb7',
-            color: 'white',             
-          }}
-        >
-          Resume
-        </Button>
+            size="xl"
+            component="a"
+            href="https://jessicaliu06.github.io/resume/LiuJessica_Resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            variant="outline"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            style={{
+              borderColor: '#edacb7',
+              borderWidth: '2.5px',
+              borderRadius: 50,
+              color: '#edacb7',
+              display: 'flex',
+              alignItems: 'center',
+              paddingLeft: 15, paddingRight: 15
+            }}
+          >
+            <img
+              src={glass_apple}
+              width={34}
+              height={34}
+              style={{
+                marginRight: '10px',
+                animation: isHovered ? 'spin 3s linear infinite' : 'none',
+              } as React.CSSProperties}
+            />
+            Resume
+          </Button>
       </Stack>
-    </Container>    
+    </Container>  
+    
+    <style>
+      {`
+        @keyframes spin {
+          from { transform: rotate(0deg); }
+          to { transform: rotate(360deg); }
+        }
+      `}
+    </style>
+    </>
   );
 }
